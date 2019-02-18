c21414619dcbcaaa64caddf471235d29b1a0afe9
Debajyoti Dutta <debajyotidofficial@gmail.com>
2019-02-18 22:15:49 +0530

    More Broken Core.

--- main.cpp
+++ main.cpp
@@ -14,17 +14,31 @@ int main ()
     ifstream infile;
     infile.open("portrait.raw12", ios::binary | ios::in);
     ofstream outfile;
-    outfile.open("Redone.ppm", ios::binary);
-    outfile << "P6\n" << 1536 << "\n" << 2048 << "\n255\n";
+    outfile.open("Redtwo.ppm", ios::binary);
+    /*
+    outfile.write("P6 ", 3);
+    outfile.write("1536 2048 ", 8);
+    outfile.write("2048 ", 4);
+    outfile.write("255 ", 4);
+    */
+    //outfile << "P6\n" << 1536 << "\n" << 2048 << "\n255\n";
+    outfile << "P6"     << "\n"
+        << 1536  << " "
+        << 2048  << "\n"
+        << 255   << "\n"
+       ;
     uint8_t * bufferRow = new uint8_t[BUFFERSIZE];
 
     if(!infile)
     {
         cout<<"Failed to open"<<endl;
     }
-    unsigned int RedChannel,GreenChannel_1,GreenChannel_2,BlueChannel;
+    int size=1536*2048*3;
+    char * RedChannel=new char[size];
+    int GreenChannel_1,GreenChannel_2,BlueChannel;
     int rowNum=0;
     int i=0;
+    int j=0;
     int pixel=1;
     while(rowNum<3072)
     {
@@ -33,27 +47,29 @@ int main ()
         {
             while(i<BUFFERSIZE)
             {
-                RedChannel=(uint8_t)bufferRow[i];
+                RedChannel[j]=(uint8_t)bufferRow[i];
                 GreenChannel_1=((uint8_t)(bufferRow[i+1] & 0x0F) << 4) | ((uint8_t)(bufferRow[i+2] >> 4) & 0x0F);
                 i+=3;
                 Collect s;
-                s.r=(char)RedChannel;
-                s.g=(char)0;
-                s.b=(char)0;
-                unsigned char c = (unsigned char)(255.0f * (float)RedChannel + 0.5f); 
+                //s.r=(char)RedChannel[j];
+                //s.g=(char)0;
+                //s.b=(char)0;
+                //unsigned char c = (unsigned char)(255.0f * (float)RedChannel[j] + 0.5f); 
                 //outfile.write((char*) &c, 3);
                 //outfile.write((char*) 255, sizeof(c));
-                outfile.write(reinterpret_cast<char* >(&s), sizeof(Collect));
+                //outfile.write(reinterpret_cast<char*>(&RedChannel), 4);
                 if(pixel<=3 && rowNum<5)
                 {
-                    cout<<"RedChannel: "<<RedChannel<<endl;
+                    cout<<"RedChannel: "<<RedChannel[j]<<endl;
                     if(pixel!=3)
                         cout<<"GreenChannel 1: "<<GreenChannel_1<<endl;
                 }
                 pixel++;
+                j++;
 
             }
-            outfile<<"\n";
+            RedChannel[j]='\n';
+            j++;
         
         }
         else
@@ -79,6 +95,7 @@ int main ()
             cout<<" "<<endl;
     }
     infile.close();
+    outfile.write(RedChannel, size);
     outfile.close();
 }
     
